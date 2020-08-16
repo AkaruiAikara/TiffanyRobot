a86343814f566149b0d21cf0787f5cd05b3b2a3b
Fallavall <fallahandyprakasa26@gmail.com>
2020-08-16 18:49:32 +0700

    __main__.py: changed /settings description

--- tiffany/__main__.py
+++ tiffany/__main__.py
@@ -41,7 +41,7 @@ Disaster module from [SaitamaRobot](https://github.com/AnimeKaizoku/SaitamaRobot
  - /help <module name>: PM's you info about that module.
  - /donate: information about how to donate!
  - /settings:
-   - in PM: will send you your settings for all supported modules.
+   - in PM: it will be fix soon :)
    - in a group: will redirect you to pm, with all that chat's settings.
 
 {}
